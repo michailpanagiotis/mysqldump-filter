40e4cf17cdd24151551f4724a85fba7652601ac5
Panos Michail <michailpanagiotis@gmail.com>
2025-03-20 10:38:16 +0100

    WIP

--- src/main.rs
+++ src/main.rs
@@ -4,6 +4,8 @@ mod splitter;
 
 fn main() {
     let (input_path, requested_tables) = options::parse_options();
-    let schema_file = String::from("schema.sql");
-    let _exported_tables = splitter::split(&input_path, &schema_file, &requested_tables);
+    // let schema_file = String::from("schema.sql");
+    // let _exported_tables = splitter::split(&input_path, &schema_file, &requested_tables);
+    let table_file = String::from("service_orders.test.sql");
+    reader::read_ids(&table_file);
 }
--- src/reader.rs
+++ src/reader.rs
@@ -1,6 +1,17 @@
+use std::collections::HashSet;
 use std::fs::File;
 use std::io::{self, BufRead};
+use std::str::FromStr;
 use std::path::Path;
+use lazy_static::lazy_static;
+use regex::Regex;
+use fastbloom::BloomFilter;
+
+lazy_static! {
+    static ref INSERT_RE: Regex = Regex::new(r"INSERT[^(]*\(([^)]+)\)").unwrap();
+    static ref INSERT_VALUES_RE: Regex = Regex::new(r"INSERT.*\(([^)]+)\)").unwrap();
+    static ref SPLIT_VALUES_RE: Regex = Regex::new(r"('[^']+')|([^,]*)").unwrap();
+}
 
 // The output is wrapped in a Result to allow matching on errors.
 // Returns an Iterator to the Reader of the lines of the file.
@@ -8,4 +19,39 @@ pub fn read_lines<P>(filename: P) -> io::Lines<io::BufReader<File>>
 where P: AsRef<Path>, {
     let file = File::open(filename).expect("Cannot open file");
     io::BufReader::new(file).lines()
+}
+
+fn parse_id<T: FromStr>(id: &str) -> Option<T> {
+    id.parse::<T>().ok()
+}
+
+pub fn read_ids(filename: &String) -> (HashSet<i32>, BloomFilter) {
+    let lines = read_lines(filename);
+    let mut id_position: Option<usize> = None;
+    let mut ids: HashSet<i32> = HashSet::new();
+    for line in lines.map_while(Result::ok) {
+        if !line.starts_with("INSERT") {
+            continue
+        }
+
+        if id_position.is_none() {
+            let fields_string = INSERT_RE.captures(&line).unwrap().get(1).unwrap().as_str().to_string();
+            let mut fields = fields_string.split(", ");
+            id_position = fields.position(|x| x.starts_with("`id`"));
+        }
+
+        println!("Reading table {}", line);
+        let values = INSERT_VALUES_RE.captures(&line).unwrap().get(1).unwrap().as_str().to_string();
+
+        println!("{}", values);
+        let id: &str = SPLIT_VALUES_RE.find_iter(values.as_str())
+            .map(|x| x.as_str())
+            .nth(id_position.unwrap()).unwrap();
+        let parsed = parse_id::<i32>(id).unwrap();
+        dbg!(parsed);
+        ids.insert(parsed);
+    }
+
+    let ids_lookup = BloomFilter::with_false_pos(0.001).items(ids.iter());
+    (ids, ids_lookup)
 }
