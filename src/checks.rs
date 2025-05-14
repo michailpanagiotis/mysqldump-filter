a08678d7047ba9cbca0286eae6d74eed4c13f38e
Panos Michail <michailpanagiotis@gmail.com>
2025-05-14 09:30:59 +0200

    --wip-- [skip ci]

--- src/checks.rs
+++ src/checks.rs
@@ -4,6 +4,14 @@ use chrono::NaiveDateTime;
 use std::collections::{HashMap, HashSet};
 use std::sync::Arc;
 
+pub trait AllowValue {
+    fn get_column_name_from_definition(definition: &str) -> String;
+    fn from_definition(definition: &str, data_type: sqlparser::ast::DataType) -> Self;
+    fn get_column_name(&self) -> &str;
+    fn test(&self, value:&str, lookup_table: &Option<HashMap<String, HashSet<String>>>) -> bool;
+    fn extend_allowed_values(&mut self);
+}
+
 #[derive(Debug)]
 pub struct CelTest {
     column: String,
@@ -27,23 +35,6 @@ fn timestamp(This(s): This<Arc<String>>) -> i64 {
 }
 
 impl CelTest {
-    pub fn get_column_name(definition: &str) -> String {
-        let program = Program::compile(definition).expect("cannot compile CEL");
-        program.references().variables().iter().map(|f| f.to_string()).next().expect("cannot find variable")
-    }
-
-    pub fn new(definition: &str, data_type: sqlparser::ast::DataType) -> Self {
-        let program = Program::compile(definition).unwrap();
-        let variables: Vec<String> = program.references().variables().iter().map(|f| f.to_string()).collect();
-        let column = &variables[0];
-
-        CelTest {
-            column: column.to_string(),
-            program,
-            data_type,
-        }
-    }
-
     fn build_context(&self, other_value: &str) -> Context {
         let mut context = Context::default();
         context.add_function("timestamp", timestamp);
@@ -68,9 +59,32 @@ impl CelTest {
 
         context
     }
+}
 
-    pub fn test(&self, other_value: &str) -> bool {
-        let context = self.build_context(other_value);
+impl AllowValue for CelTest {
+    fn get_column_name_from_definition(definition: &str) -> String {
+        let program = Program::compile(definition).expect("cannot compile CEL");
+        program.references().variables().iter().map(|f| f.to_string()).next().expect("cannot find variable")
+    }
+
+    fn from_definition(definition: &str, data_type: sqlparser::ast::DataType) -> Self {
+        let program = Program::compile(definition).unwrap();
+        let variables: Vec<String> = program.references().variables().iter().map(|f| f.to_string()).collect();
+        let column = &variables[0];
+
+        CelTest {
+            column: column.to_string(),
+            program,
+            data_type,
+        }
+    }
+
+    fn get_column_name(&self) -> &str {
+        &self.column
+    }
+
+    fn test(&self, value:&str, lookup_table: &Option<HashMap<String, HashSet<String>>>) -> bool {
+        let context = self.build_context(value);
         match self.program.execute(&context).unwrap() {
             cel_interpreter::objects::Value::Bool(v) => {
                 // println!("testing {}.{} {} -> {}", self.table, self.column, &other_value, &v);
@@ -79,48 +93,21 @@ impl CelTest {
             _ => panic!("filter does not return a boolean"),
         }
     }
+
+    fn extend_allowed_values(&mut self) {
+
+    }
 }
 
 #[derive(Debug)]
 pub struct LookupTest {
+    column: String,
     lookup_key: String,
     target_table: String,
     target_column: String,
 }
 
 impl LookupTest {
-    pub fn get_column_name(definition: &str) -> String {
-        let mut split = definition.split("->");
-        let Some(variable) = split.next() else {
-            panic!("cannot parse cascade");
-        };
-        variable.to_string()
-    }
-
-    pub fn new(definition: &str) -> Self {
-        let mut split = definition.split("->");
-        let (Some(_), Some(foreign_key), None) = (split.next(), split.next(), split.next()) else {
-            panic!("cannot parse cascade");
-        };
-
-        let mut split = foreign_key.split('.');
-        let (Some(table), Some(column), None) = (split.next(), split.next(), split.next()) else {
-            panic!("malformed foreign key {foreign_key}");
-        };
-
-        LookupTest {
-            lookup_key: foreign_key.to_string(),
-            target_table: table.to_string(),
-            target_column: column.to_string(),
-        }
-    }
-
-    pub fn test(&self, value:&str, lookup_table: &Option<HashMap<String, HashSet<String>>>) -> bool {
-        let Some(fvs) = lookup_table else { return true };
-        let Some(set) = fvs.get(self.lookup_key.as_str()) else { return false };
-        set.contains(value)
-    }
-
     pub fn get_foreign_key(&self) -> (String, String) {
         (self.target_table.clone(), self.target_column.clone())
     }
@@ -134,6 +121,48 @@ impl LookupTest {
     }
 }
 
+impl AllowValue for LookupTest {
+    fn get_column_name_from_definition(definition: &str) -> String {
+        let mut split = definition.split("->");
+        let Some(variable) = split.next() else {
+            panic!("cannot parse cascade");
+        };
+        variable.to_string()
+    }
+
+    fn from_definition(definition: &str, data_type: sqlparser::ast::DataType) -> Self {
+        let mut split = definition.split("->");
+        let (Some(_), Some(foreign_key), None) = (split.next(), split.next(), split.next()) else {
+            panic!("cannot parse cascade");
+        };
+
+        let mut split = foreign_key.split('.');
+        let (Some(table), Some(column), None) = (split.next(), split.next(), split.next()) else {
+            panic!("malformed foreign key {foreign_key}");
+        };
+
+        LookupTest {
+            column: LookupTest::get_column_name_from_definition(definition),
+            lookup_key: foreign_key.to_string(),
+            target_table: table.to_string(),
+            target_column: column.to_string(),
+        }
+    }
+
+    fn get_column_name(&self) -> &str {
+        &self.column
+    }
+
+    fn test(&self, value:&str, lookup_table: &Option<HashMap<String, HashSet<String>>>) -> bool {
+        let Some(fvs) = lookup_table else { return true };
+        let Some(set) = fvs.get(self.lookup_key.as_str()) else { return false };
+        set.contains(value)
+    }
+
+    fn extend_allowed_values(&mut self) {
+
+    }
+}
 
 #[derive(Debug)]
 pub enum ValueTest {
@@ -150,14 +179,18 @@ pub struct ColumnTest {
 }
 
 impl ColumnTest {
+    // pub resolve_inner_test() -> ValueTest {
+    //
+    // }
+
     pub fn new_cel(table: &str, condition: &str, data_types: &HashMap<String, sqlparser::ast::DataType>) -> Self {
-        let column = CelTest::get_column_name(condition);
+        let column = CelTest::get_column_name_from_definition(condition);
         let data_type = match data_types.get(&(table.to_owned() + "." + &column)) {
             None => panic!("{}", format!("cannot find data type for {table}.{column}")),
             Some(data_type) => data_type.to_owned()
         };
 
-        let condition = CelTest::new(condition, data_type);
+        let condition = CelTest::from_definition(condition, data_type);
         ColumnTest {
             table: table.to_owned(),
             column,
@@ -166,9 +199,13 @@ impl ColumnTest {
         }
     }
 
-    pub fn new_lookup(table: &str, condition: &str) -> Self {
-        let column = LookupTest::get_column_name(condition);
-        let condition = LookupTest::new(condition);
+    pub fn new_lookup(table: &str, condition: &str, data_types: &HashMap<String, sqlparser::ast::DataType>) -> Self {
+        let column = LookupTest::get_column_name_from_definition(condition);
+        let data_type = match data_types.get(&(table.to_owned() + "." + &column)) {
+            None => panic!("{}", format!("cannot find data type for {table}.{column}")),
+            Some(data_type) => data_type.to_owned()
+        };
+        let condition = LookupTest::from_definition(condition, data_type);
         ColumnTest {
             table: table.to_owned(),
             column,
@@ -184,7 +221,7 @@ impl ColumnTest {
             }));
         let cascade_iter = cascades.iter()
             .flat_map(|(table, conditions)| conditions.iter().map(|condition| {
-                ColumnTest::new_lookup(table, condition)
+                ColumnTest::new_lookup(table, condition, data_types)
             }));
 
         let mut collected: Vec<ColumnTest> = filter_iter.chain(cascade_iter).collect();
@@ -195,7 +232,7 @@ impl ColumnTest {
 
     pub fn test(&self, value: &str, lookup_table: &Option<HashMap<String, HashSet<String>>>) -> bool {
         match &self.test {
-            ValueTest::Cel(cond) => cond.test(value),
+            ValueTest::Cel(cond) => cond.test(value, lookup_table),
             ValueTest::Cascade(cond) => cond.test(value, lookup_table),
         }
     }
