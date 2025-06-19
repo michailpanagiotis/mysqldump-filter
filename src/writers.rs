a29c6d2f013933f7f816d92a1cd79a7915c0e92f
Panos Michail <michailpanagiotis@gmail.com>
2025-06-19 13:25:01 +0200

    Stable

--- src/writers.rs
+++ src/writers.rs
@@ -39,32 +39,21 @@ impl Writers {
         std::path::absolute(self.working_dir_path.join(table).with_extension("sql"))
     }
 
-    fn determine_output_file(&self, table_option: &Option<String>) -> Result<PathBuf, anyhow::Error> {
-        match table_option {
-            None => {
-                if self.in_place {
-                    return Err(anyhow::anyhow!("cannot write to working file in place"));
-                }
-                Ok(self.working_file_path.to_owned())
+    fn determine_output_file(&self, table: &str) -> Result<PathBuf, anyhow::Error> {
+        let table_file = std::path::absolute(
+            if self.in_place {
+                self.working_dir_path.join(table).with_extension("proc")
+            } else {
+                self.working_dir_path.join(table).with_extension("sql")
             }
-            Some(table) => {
-                let table_file = std::path::absolute(
-                    if self.in_place {
-                        self.working_dir_path.join(table).with_extension("proc")
-                    } else {
-                        self.working_dir_path.join(table).with_extension("sql")
-                    }
-                )?;
-                Ok(table_file)
-            }
-        }
+        )?;
+        Ok(table_file)
     }
 
-    fn determine_writer(&mut self, table_option: &Option<String>) -> EmptyResult {
-        if self.current_writer.is_none() || table_option != &self.current_table {
-            println!("determining writer");
-            self.current_table = table_option.clone();
-            let filepath = self.determine_output_file(table_option)?;
+    fn determine_writer(&mut self, table: &str) -> EmptyResult {
+        if self.current_writer.is_none() || Some(table) != self.current_table.as_deref() {
+            self.current_table = Some(table.to_owned());
+            let filepath = self.determine_output_file(table)?;
             self.current_file = Some(filepath.to_owned());
             if !self.written_files.contains(&filepath) {
                 println!("creating file {}", &filepath.display());
@@ -79,43 +68,55 @@ impl Writers {
             }
             self.current_writer = Some(BufWriter::new(file));
         }
-        if self.current_table.is_none() && self.working_file_writer.is_none() {
-            println!("determining working file writer");
-            let file = fs::OpenOptions::new().append(true).open(&self.working_file_path)?;
-            self.working_file_writer = Some(BufWriter::new(file));
-        }
         Ok(())
     }
 
-    fn try_write_inline_file(&mut self, table_option: &Option<String>, filepath: &Path) -> EmptyResult {
+    fn try_write_inline_file(&mut self, table: &str, filepath: &Path) -> EmptyResult {
         if !self.inline_files.contains(filepath) {
             self.inline_files.insert(filepath.to_owned());
-            println!("inlining file {}", &filepath.display());
-            if let Some(table) = table_option {
-                let Some(ref mut working_file_writer) = self.working_file_writer else {
-                    return Err(anyhow::anyhow!("cannot find output file"));
-                };
-                working_file_writer.write_all(format!("--- INLINE {} {}\n", filepath.display(), table).as_bytes())?;
+            let Some(ref mut working_file_writer) = self.working_file_writer else {
+                return Err(anyhow::anyhow!("cannot find output file"));
             };
+            working_file_writer.write_all(format!("--- INLINE {} {}\n", filepath.display(), table).as_bytes())?;
         }
         Ok(())
     }
 
     pub fn write_statement(&mut self, table_option: &Option<String>, statement: &[u8]) -> EmptyResult {
-        self.determine_writer(table_option)?;
-        let filepath_option = self.current_file.to_owned();
-        let Some(writer) = &mut self.current_writer else {
-            return Err(anyhow::anyhow!("cannot find writer"));
-        };
-        let Some(filepath) = &filepath_option else {
-            return Err(anyhow::anyhow!("cannot find output file"));
-        };
+        match table_option {
+            Some(table) => {
+                self.determine_writer(table)?;
+                let filepath_option = self.current_file.to_owned();
+                let Some(writer) = &mut self.current_writer else {
+                    return Err(anyhow::anyhow!("cannot find writer"));
+                };
+                let Some(filepath) = &filepath_option else {
+                    return Err(anyhow::anyhow!("cannot find output file"));
+                };
 
-        writer.write_all(statement)?;
+                writer.write_all(statement)?;
 
-        if !self.in_place {
-            self.try_write_inline_file(table_option, filepath)?;
+                if !self.in_place {
+                    if let Some(table) = table_option {
+                        self.try_write_inline_file(table, filepath)?;
+                    }
+                }
+            },
+            None => {
+                if self.working_file_writer.is_none() {
+                    println!("determining working file writer");
+                    fs::File::create(&self.working_file_path)?;
+                    let file = fs::OpenOptions::new().append(true).open(&self.working_file_path)?;
+                    self.working_file_writer = Some(BufWriter::new(file));
+                }
+
+                let Some(writer) = &mut self.working_file_writer else {
+                    return Err(anyhow::anyhow!("cannot find working file writer"));
+                };
+                writer.write_all(statement)?;
+            }
         }
+
         Ok(())
     }
 
