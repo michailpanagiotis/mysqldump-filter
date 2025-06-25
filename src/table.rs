4b07aee51bb662dbb0fd6622a7288f51d3c1cde0
Panos Michail <michailpanagiotis@gmail.com>
2025-06-25 19:17:39 +0200

    --wip-- [skip ci]

--- src/table.rs
+++ src/table.rs
@@ -28,43 +28,6 @@ fn process_inserts(
 }
 
 type TableMetaCell = Rc<RefCell<TableMeta>>;
-type DependencyCell = Rc<RefCell<Dependency>>;
-type WeakDependencyRef = Weak<RefCell<Dependency>>;
-
-#[derive(Debug)]
-#[derive(Default)]
-struct Dependency {
-    dependencies: Vec<WeakDependencyRef>,
-    tested_at_pass: Option<usize>,
-}
-
-impl Dependency {
-    fn new() -> Rc<RefCell<Self>> {
-        Rc::new(RefCell::new(Dependency { dependencies: Vec::new(), tested_at_pass: None }))
-    }
-
-    fn add_dependency(&mut self, target: &Rc<RefCell<Dependency>>) {
-        let weak = Rc::downgrade(target);
-        self.dependencies.push(weak);
-    }
-
-    fn has_been_fulfilled(&self) -> bool {
-        self.tested_at_pass.is_some()
-    }
-
-    fn has_fulfilled_dependencies(&self) -> bool {
-        self.dependencies.iter().all(|d| {
-            d.upgrade().unwrap().borrow().has_been_fulfilled()
-        })
-    }
-
-    fn fulfill_dependency(&mut self, depth: &usize) {
-        if !self.has_been_fulfilled() {
-            self.tested_at_pass = Some(depth.to_owned());
-        }
-        assert!(self.has_been_fulfilled());
-    }
-}
 
 
 #[derive(Debug)]
@@ -74,7 +37,6 @@ pub struct TableMeta {
     foreign_tables: Vec<String>,
     references: Vec<String>,
     checks: Vec<PlainCheckType>,
-    dependency: DependencyCell,
 }
 
 impl TryFrom<&TableChecks> for TableMetaCell {
@@ -86,7 +48,6 @@ impl TryFrom<&TableChecks> for TableMetaCell {
             foreign_tables: table_checks.foreign_tables.clone(),
             references: table_checks.references.clone(),
             checks,
-            dependency: Dependency::new(),
         })))
     }
 }
@@ -100,25 +61,12 @@ impl TableMeta {
         self.references.iter().map(|x| x.as_str()).collect()
     }
 
-    fn add_dependency(&mut self, target: &Rc<RefCell<TableMeta>>) {
-        self.dependency.borrow_mut().add_dependency(&target.borrow().dependency);
-    }
-
-    pub fn process_data_file(
+    fn process_data_file(
         &mut self,
-        current_pass: &usize,
         lookup_table: &HashMap<String, HashSet<String>>,
         working_file_path: &Path,
     ) -> Result<Option<HashMap<String, HashSet<String>>>, anyhow::Error> {
-        if !self.dependency.borrow().has_fulfilled_dependencies() {
-            println!("Skipping table {} since it still has dependencies", &self.table);
-            return Ok(None);
-        }
-
         let captured = process_inserts(working_file_path, &self.table, &self.checks, &self.get_tracked_columns(), lookup_table)?;
-
-        self.dependency.borrow_mut().fulfill_dependency(current_pass);
-
         Ok(Some(captured))
     }
 }
@@ -126,6 +74,7 @@ impl TableMeta {
 #[derive(Debug)]
 pub struct CheckCollection {
     table_meta: HashMap<String, Rc<RefCell<TableMeta>>>,
+    grouped_tables: Vec<HashSet<String>>,
 }
 
 impl CheckCollection {
@@ -147,8 +96,6 @@ impl CheckCollection {
 
         dbg!(&root);
         dbg!(&root.group_by_depth());
-        panic!("stop");
-
 
         let checks_per_table = get_checks_per_table(&definitions)?;
 
@@ -157,22 +104,10 @@ impl CheckCollection {
             grouped.insert(table.to_owned(), TableMetaCell::try_from(checks)?);
         }
 
-        // set dependencies
-        for table_meta in grouped.values() {
-            let foreign_tables = table_meta.borrow().get_foreign_tables();
-            for target_table in foreign_tables.iter() {
-                let target_table_meta = &grouped[target_table];
-                table_meta.borrow_mut().add_dependency(target_table_meta);
-            }
-        }
-
         Ok(CheckCollection {
             table_meta: grouped,
+            grouped_tables: root.group_by_depth(),
         })
-    }
-
-    fn get_pending_tables(&self) -> Vec<String>{
-        self.table_meta.values().filter(|v| !v.borrow().dependency.borrow().has_been_fulfilled()).map(|v| v.borrow().table.to_owned()).collect()
     }
 
     pub fn process(
@@ -181,14 +116,12 @@ impl CheckCollection {
     ) -> Result<(), anyhow::Error> {
         let mut current_pass = 1;
         let mut lookup_table = HashMap::new();
-        while !self.get_pending_tables().is_empty() {
-            let pending = self.get_pending_tables();
+        for pending in self.grouped_tables.iter() {
             println!("Running pass {current_pass}");
             dbg!(&pending);
             dbg!(&lookup_table);
             for table_meta in self.table_meta.values_mut().filter(|t| pending.iter().any(|p| p == &t.borrow().table)) {
                 let captured_option = table_meta.borrow_mut().process_data_file(
-                    &current_pass,
                     &lookup_table,
                     working_file_path,
                 )?;
