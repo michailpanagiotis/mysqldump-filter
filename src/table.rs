65a5786d196fdfc6ea4ce805b2afc62b8d7092f4
Panos Michail <michailpanagiotis@gmail.com>
2025-07-01 10:24:58 +0200

    --wip-- [skip ci]

--- src/table.rs
+++ src/table.rs
@@ -1,18 +1,28 @@
-use std::collections::HashMap;
+use std::collections::{HashMap, HashSet};
 use std::path::Path;
 
-use crate::checks::DBChecks;
-use crate::scanner::process_table_inserts;
+use crate::checks::{DBChecks, TableChecks};
+use crate::scanner::{TransformFn, process_table_inserts};
+
+pub fn process_inserts<F>(
+    checks: &TableChecks,
+    working_file_path: &Path,
+    transform: F,
+) -> Result<HashMap<String, HashSet<String>>, anyhow::Error>
+  where F: TransformFn
+{
+    let tracked_columns = checks.get_tracked_columns();
+    let table = checks.get_table()?;
+    let captured = process_table_inserts(table, &tracked_columns, working_file_path, transform)?;
+    Ok(captured)
+}
 
 pub fn process_checks(passes: DBChecks, working_file_path: &Path) -> Result<(), anyhow::Error> {
     let mut lookup_table = HashMap::new();
     for pending in passes.0 {
         dbg!(&lookup_table);
         for checks in pending {
-            let tracked_columns = checks.get_tracked_columns();
-            let table = checks.get_table()?;
-
-            let captured = process_table_inserts(table, &tracked_columns, working_file_path, |statement| {
+            let captured = process_inserts(&checks, working_file_path, |statement| {
                 let value_per_field = statement.get_values()?;
                 checks.test(value_per_field, &lookup_table)
             })?;
