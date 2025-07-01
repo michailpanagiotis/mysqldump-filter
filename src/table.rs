27b94e704cdf65109ac5c08a2badc7c3f7eab092
Panos Michail <michailpanagiotis@gmail.com>
2025-07-01 10:13:43 +0200

    --wip-- [skip ci]

--- src/table.rs
+++ src/table.rs
@@ -4,7 +4,6 @@ use std::path::Path;
 use crate::checks::DBChecks;
 use crate::scanner::process_table_inserts;
 
-
 pub fn process_checks(passes: DBChecks, working_file_path: &Path) -> Result<(), anyhow::Error> {
     let mut lookup_table = HashMap::new();
 
@@ -16,7 +15,6 @@ pub fn process_checks(passes: DBChecks, working_file_path: &Path) -> Result<(), 
         for checks in pending.0.iter() {
             let tracked_columns = checks.get_tracked_columns();
             let table = checks.get_table()?;
-
 
             let captured = process_table_inserts(working_file_path, table, &tracked_columns, |statement| {
                 let value_per_field = statement.get_values()?;
