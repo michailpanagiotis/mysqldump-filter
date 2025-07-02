9e4fc24832eeca9e114814fc0a07987668a84e37
Panos Michail <michailpanagiotis@gmail.com>
2025-07-02 09:57:41 +0200

    --wip-- [skip ci]

--- src/table.rs
+++ src/table.rs
@@ -4,11 +4,8 @@ use std::path::Path;
 use crate::checks::{DBChecks, TableChecks};
 use crate::scanner::{TransformFn, process_table_inserts};
 
-fn get_transform<'a>(
-    table_checks: &'a TableChecks,
-    lookup_table: &'a HashMap<String, HashSet<String>>,
-) -> Box<dyn TransformFn + 'a>{
-    Box::new(|statement| {
+fn get_table_transform(table_checks: &TableChecks, lookup_table: &HashMap<String, HashSet<String>>) -> impl TransformFn {
+    |statement| {
         let Ok(value_per_field) = statement.try_into() else { Err(anyhow::anyhow!("cannot parse values"))? };
         for check in table_checks.0.iter() {
             if !check.test(value_per_field, lookup_table)? {
@@ -16,7 +13,7 @@ fn get_transform<'a>(
             }
         }
         Ok(Some(HashMap::new()))
-    })
+    }
 }
 
 pub fn process_checks(passes: DBChecks, working_file_path: &Path) -> Result<(), anyhow::Error> {
@@ -30,7 +27,7 @@ pub fn process_checks(passes: DBChecks, working_file_path: &Path) -> Result<(), 
                 table,
                 &tracked_columns,
                 working_file_path,
-                get_transform(&table_checks, &lookup_table),
+                get_table_transform(&table_checks, &lookup_table),
             )?;
 
             lookup_table.extend(captured);
